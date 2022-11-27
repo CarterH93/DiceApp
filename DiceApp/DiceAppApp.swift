ded335390f20b5a4f425dc6298c69612f7a5f903
CarterH93 <Carterhawkins93@gmail.com>
2022-11-26 19:56:41 -0500

    Initial Commit

---
+++ DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by Carter Hawkins on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
